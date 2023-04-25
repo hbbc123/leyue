32f77e7e277b5e76bb938c25ba236f26f0f083ff
hbbc123 <2411590445@qq.com>
2023-04-25 19:24:12 +0800

    项目文件

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-0ge4frqj3f10a146","alias":"cloud1"},{"envId":"leyue-8gmqvf2630ca980b","alias":"leyue"},{"envId":"leyue-4g4gqg02df1e27e2","alias":"leyue"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
